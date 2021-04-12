6fd807b7a68a33b96fbc755671c1e8e541b11cd9
Zaur Malakhov <1zaur1@mail.ru>
2021-04-12 07:40:21 +0300

    3.1.9 Переведет film popup на классы

--- src/main.js
+++ src/main.js
@@ -1,71 +1,56 @@
 import {positionsToInsertElement} from './util/const.js';
 import {getSortFilms, sortFilmsByRating, sortFilmsByCommetns, renderElement} from './util/utils.js';
-// import {createProfileTemplate} from './view/profile.js';
 import ProfileView from './view/profile.js';
-import {generateFilter} from './mock/filter.js';
-// import {createSiteMenuTemplate} from './view/site-menu.js';
 import SiteMenuView from './view/site-menu.js';
-// import {createFilterTemplate} from './view/filter.js';
 import FilterView from './view/filter.js';
-// import {createSortTemplate} from './view/sort.js';
 import SortView from './view/sort.js';
-// import {createFilmsTemplate} from './view/films.js';
 import FilmsView from './view/films.js';
-// import {createFilmTemplate} from './view/film-card.js';
 import FilmCardView from './view/film-card.js';
-// import {createButtonMoreTemplate} from './view/more-button.js';
 import LoadMoreButtonView from './view/more-button.js';
-// import {createFilmsStatisticsTemplate} from './view/films-statistics.js';
 import FilmsStatisticsView from './view/films-statistics.js';
 import {generateFilm} from './mock/film.js';
+import {generateFilter} from './mock/filter.js';
 // import {getComments} from './mock/comment.js';
+import FilmPopupView from './view/film-popup.js';
 import {
   FILM_COUNT_ALL_MOVIES,
   FILM_COUNT_PER_STEP,
   FILM_COUNT_TOP_RATED,
   FILM_COUNT_MOST_COMMENTED
 } from './util/const.js';
-// import {createFilmPopupTemplate} from './view/film-popup.js';
+
 
 // films list
 const films = new Array(FILM_COUNT_ALL_MOVIES).fill().map(generateFilm);
-// console.log(films);
 
 // filters list
 const filters = generateFilter(films);
-// console.log(filters);
 
 const filmsTopRated = getSortFilms(films, sortFilmsByRating);
 const filmsMostCommented = getSortFilms(films, sortFilmsByCommetns);
 
-
-// const siteBodyElement = document.querySelector('body');
-// render(siteBodyElement, createFilmPopupTemplate(films[0]));
+const siteBodyElement = document.querySelector('body');
+renderElement(siteBodyElement, new FilmPopupView(films[0]).getElement());
 
 // user profile
 const siteHeaderElement = document.querySelector('.header');
-// renderTemplate(siteHeaderElement, createProfileTemplate());
 renderElement(siteHeaderElement, new ProfileView().getElement());
 
 
 const siteMainElement = document.querySelector('.main');
 
 // main navigation
-// renderTemplate(siteMainElement, createSiteMenuTemplate());
 renderElement(siteMainElement, new SiteMenuView().getElement());
 
 
 // filters in main navigation
 const mainNavigationElement = document.querySelector('.main-navigation');
-// renderTemplate(mainNavigationElement, createFilterTemplate(filters), positionsToInsertElement.AFTERBEGIN);
 renderElement(mainNavigationElement, new FilterView(filters).getElement(), positionsToInsertElement.AFTERBEGIN);
 
 // sort
-// renderTemplate(siteMainElement, createSortTemplate());
 renderElement(siteMainElement, new SortView().getElement());
 
 // films
-// renderTemplate(siteMainElement, createFilmsTemplate());
 renderElement(siteMainElement, new FilmsView().getElement());
 
 const filmsElement = siteMainElement.querySelector('.films');
@@ -76,13 +61,11 @@ const filmListMostCommented = filmsElement.querySelector('.films-list--most-comm
 // All movies
 const filmListContainerAllMovies = filmListAllMovies.querySelector('.films-list__container');
 for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
-  // renderTemplate(filmListContainerAllMovies, createFilmTemplate(films[i]));
   renderElement(filmListContainerAllMovies, new FilmCardView(films[i]).getElement());
 }
 if (films.length > FILM_COUNT_PER_STEP) {
   let renderedFilmCount = FILM_COUNT_PER_STEP;
 
-  // renderTemplate(filmListAllMovies, createButtonMoreTemplate());
   renderElement(filmListAllMovies, new LoadMoreButtonView().getElement());
 
   const showMoreButton = filmListAllMovies.querySelector('.films-list__show-more');
@@ -91,7 +74,6 @@ if (films.length > FILM_COUNT_PER_STEP) {
     evt.preventDefault();
     films
       .slice(renderedFilmCount, renderedFilmCount + FILM_COUNT_PER_STEP)
-      // .forEach((film) => renderTemplate(filmListContainerAllMovies, createFilmTemplate(film)));
       .forEach((film) => renderElement(filmListContainerAllMovies, new FilmCardView(film).getElement()));
 
     renderedFilmCount += FILM_COUNT_PER_STEP;
@@ -102,22 +84,18 @@ if (films.length > FILM_COUNT_PER_STEP) {
   });
 }
 
-
 // Top rated
 const filmListContainerTopRated = filmListTopRated.querySelector('.films-list__container');
 for (let i = 0; i < FILM_COUNT_TOP_RATED; i++) {
-  // renderTemplate(filmListContainerTopRated, createFilmTemplate(filmsTopRated[i]));
   renderElement(filmListContainerTopRated, new FilmCardView(filmsTopRated[i]).getElement());
 }
 
 // Most commented
 const filmListContainerMostCommented = filmListMostCommented.querySelector('.films-list__container');
 for (let i = 0; i < FILM_COUNT_MOST_COMMENTED; i++) {
-  // renderTemplate(filmListContainerMostCommented, createFilmTemplate(filmsMostCommented[i]));
   renderElement(filmListContainerMostCommented, new FilmCardView(filmsMostCommented[i]).getElement());
 }
 
 const siteFooterElement = document.querySelector('.footer');
 const siteFooterStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
-// renderTemplate(siteFooterStatisticsElement, createFilmsStatisticsTemplate(films));
 renderElement(siteFooterStatisticsElement, new FilmsStatisticsView(films).getElement());
--- src/view/film-popup.js
+++ src/view/film-popup.js
@@ -1,8 +1,9 @@
 import {getComments} from '../mock/comment.js';
 import {formatDate, getTimeDuration} from '../util/utils.js';
 import {dataFormat} from '../util/const.js';
+import {createElement} from '../util/utils.js';
 
-export const createFilmPopupTemplate = (film) => {
+const createFilmPopupTemplate = (film) => {
 
   const commentsList = getComments();
 
@@ -147,3 +148,27 @@ export const createFilmPopupTemplate = (film) => {
   </form>
 </section>`;
 };
+
+export default class FilmPopup {
+  constructor(film) {
+    this._film = film;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createFilmPopupTemplate(this._film);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
