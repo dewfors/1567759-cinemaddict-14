562ea73aadc97a21ede9b583142c9e27efe86c9c
Zaur Malakhov <1zaur1@mail.ru>
2021-05-17 08:43:46 +0300

    8-1-6 Отрефакторит инициализацию приложения

--- src/main.js
+++ src/main.js
@@ -1,26 +1,19 @@
 import {FILM_COUNT_ALL_MOVIES} from './util/const.js';
 import {render} from './util/render.js';
 import Api from './model/api.js';
-// import {getSortFilms, sortFilmsByRating, sortFilmsByCommetns} from './util/film.js';
-// import ProfileView from './view/profile.js';
-// import SiteMenuView from './view/site-menu.js';
-// import FilterView from './view/filter.js';
-
 import FilmsStatisticsView from './view/films-statistics.js';
 import {generateFilm} from './mock/film.js';
-// import {generateFilter} from './mock/filter.js';
 import FilmsModel from './model/films.js';
 import FilterModel from './model/filter.js';
 import CommentsModel from './model/comments.js';
 import MovieListPresenter from './presenter/movie-list.js';
 import FilterPresenter from './presenter/filter.js';
 import {getComments} from './mock/comment.js';
-// import {MenuItem} from './util/const.js';
 
 
 // films list
-const films = new Array(FILM_COUNT_ALL_MOVIES).fill().map(generateFilm);
-console.log(films);
+// const films = new Array(FILM_COUNT_ALL_MOVIES).fill().map(generateFilm);
+// console.log(films);
 const comments = getComments();
 // const filmsTopRated = getSortFilms(films, sortFilmsByRating);
 // const filmsMostCommented = getSortFilms(films, sortFilmsByCommetns);
@@ -30,16 +23,16 @@ const END_POINT = 'https://14.ecmascript.pages.academy/cinemaddict';
 
 const api = new Api(END_POINT, AUTHORIZATION);
 
-api.getFilms().then((films) => {
-  console.log(films);
-  // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
-  // а ещё на сервере используется snake_case, а у нас camelCase.
-  // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
-  // Есть вариант получше - паттерн "Адаптер"
-});
+// api.getFilms().then((films) => {
+//   console.log(films);
+//   // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+//   // а ещё на сервере используется snake_case, а у нас camelCase.
+//   // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+//   // Есть вариант получше - паттерн "Адаптер"
+// });
 
 const filmsModel = new FilmsModel();
-filmsModel.setFilms(films);
+// filmsModel.setFilms(films);
 
 const filterModel = new FilterModel();
 
@@ -81,3 +74,7 @@ const siteFooterElement = document.querySelector('.footer');
 const siteFooterStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
 const filmsStatisticsComponent = new FilmsStatisticsView(films);
 render(siteFooterStatisticsElement, filmsStatisticsComponent);
+
+api.getFilms().then((films) => {
+  filmsModel.setFilms(films);
+});
